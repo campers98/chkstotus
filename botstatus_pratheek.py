f8ff79f34a777e8304890cbe5be116bccb116ce3
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2023-07-29 17:50:24 +0530

    Update botstatus_pratheek.py

--- botstatus_pratheek.py
+++ botstatus_pratheek.py
@@ -31,7 +31,7 @@ async def main_pratheek():
                     try:
                         yyy_pratheek = await app.send_message(bot, "/start")
                         aaa = yyy_pratheek.id
-                        await asyncio.sleep(10)
+                        await asyncio.sleep(2)
                         zzz_pratheek = app.get_chat_history(bot, limit = 1)
                         async for ccc in zzz_pratheek:
                             bbb = ccc.id
@@ -51,9 +51,9 @@ async def main_pratheek():
                         await asyncio.sleep(e.x)            
                 time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
                 last_update = time.strftime(f"%d %b %Y at %I:%M %p")
-                xxx_pratheek += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n**♻️ Refreshes automatically - Powered By Pratheek**"
+                xxx_pratheek += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n**♻️ Refreshes automatically**"
                 await app.edit_message_text(int(CHANNEL_ID), MESSAGE_ID, xxx_pratheek)
                 print(f"Last checked on: {last_update}")                
-                await asyncio.sleep(6300)
+                await asyncio.sleep(300)
                         
 app.run(main_pratheek())
