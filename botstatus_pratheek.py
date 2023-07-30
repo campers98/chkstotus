c373873b7436ed44e4b4b0fd9c03abb73cf6754d
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2023-07-30 18:29:52 +0530

    Update botstatus_pratheek.py

--- botstatus_pratheek.py
+++ botstatus_pratheek.py
@@ -88,23 +88,14 @@ async def add_bot_handler(client: Client, message: types.Message):
         # Save the updated dictionary to environment variables
         save_bot_owners_and_logs_to_env()
 
-        # Send the /help command to the bot
-        await app.send_message(bot, "/help")
-
-        await asyncio.sleep(2)
-
-        # Get the latest message from the bot chat history
-        async for message in app.iter_history(bot, limit=1):
-            if message.outgoing and message.text == "/help":
-                xxx_pratheek += f"\n\n🤖  @{bot}\n        └ **Alive** ✅"
-            else:
-                xxx_pratheek += f"\n\n🤖  @{bot}\n        └ **Down** ❌"
-
         # Update the status message and send it to the channel
         await update_and_send_status_message()
 
         # Reply with a success message
         await message.reply(f"Added {bot} with owner ID: {owner_id} and log group ID: {log_group_id}")
+
+        # Call the main_pratheek() function again to restart the main loop
+        await main_pratheek()
     except ValueError:
         await message.reply("Invalid input. Use /addbot <bot> <owner_id> <log_group_id> format.")
         
